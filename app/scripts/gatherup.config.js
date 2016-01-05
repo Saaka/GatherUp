d01dc1ed3494db9e764f48b02996c1ddecc53b8f
Michał Sakwa <saka1988@gmail.com>
2016-01-06 00:54:56 +0100

    Added working routes.

--- app/scripts/gatherup.config.js
+++ app/scripts/gatherup.config.js
@@ -1,6 +1,18 @@
-app.config(function($stateProvider) {
-    $stateProvider.state('register', {
-       url: '/register',
-        templateUrl: '/views/register.html'
-    });
+app.config(function ($urlRouterProvider,$stateProvider) {
+
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+        .state('main', {
+            url: '/',
+            templateUrl: 'app/views/main.html'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: 'app/views/register.html'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/views/login.html'
+        });
 });
