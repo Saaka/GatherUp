c14f5b220f4a847293dcaf3a251c21ce12fe2863
Michał Sakwa <saka1988@gmail.com>
2016-01-17 20:02:40 +0100

    Changed custom alert to toastr.

--- app/scripts/controllers/registerCtrl.js
+++ app/scripts/controllers/registerCtrl.js
@@ -1,10 +1,11 @@
 'use strict';
 
-app.controller('RegisterCtrl', ['$scope', '$http', 'registerService', 'alert', function ($scope, $http, registerService, alert) {
+app.controller('RegisterCtrl', ['$scope', '$http', 'registerService', 'toaster', function ($scope, $http, registerService, toaster) {
     $scope.user = {};
-    
+
     $scope.submit = function () {
-        alert('success', 'User registered!', 'Check your email ' + $scope.user.email + ' for further instructions', 4000);
+        toaster.pop('info', 'User registered!', 'Check your email ' + $scope.user.email + ' for further instructions.', 8000);
+        //alert('success', 'User registered!', 'Check your email ' + $scope.user.email + ' for further instructions', 4000);
         //registerService.registerUser($scope.user);
     };
 }]);
